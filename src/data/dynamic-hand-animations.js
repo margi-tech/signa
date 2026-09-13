530fd8e028c53d5bd9e3436f41f16669c88d1b55
andreea1501 <andreea15010@gmail.com>
2026-09-13 17:29:19 +0300

    Adaugare animatii la literele dinamice (la lectii)

---
+++ src/data/dynamic-hand-animations.js
@@ -0,0 +1,17 @@
+import HandZ from '../components/animation/HandZ';
+import HandI2 from '../components/animation/HandI2';
+import HandS2 from '../components/animation/HandS2';
+import HandX from '../components/animation/HandX';
+import HandT2 from '../components/animation/HandT2';
+import HandJ from '../components/animation/HandJ';
+
+const DYNAMIC_HAND_ANIMATIONS = {
+    Z: HandZ,
+    Î: HandI2,
+    Ș: HandS2,
+    X: HandX,
+    Ț: HandT2,
+    J: HandJ,
+};
+
+export default DYNAMIC_HAND_ANIMATIONS;
